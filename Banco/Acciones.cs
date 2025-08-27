8429b328d2d5b77e14a438f28478f006aa5b1fb8
Minilab-ing09 <Minilab-ing09@MINILAB-ING09>
2025-08-27 08:17:52 -0600

    Agregar archivos de proyecto.

---
+++ Banco/Acciones.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    internal class Acciones
+    {
+        private double saldo;
+
+        public double Saldo()
+        {
+            return saldo;
+
+        }
+
+        public void depositar(double dep)
+        { 
+            saldo += dep;
+        }
+
+        public void retirar (double ret)
+        {
+            if (saldo> ret)
+                saldo -= ret;
+
+
+            else
+                Console.WriteLine("No tienes saldo suficiente");
+        }
+    }
+}
+
+        
---
+++ Banco/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Acciones acciones = new Acciones();
+
+            bool entrar = loggin();
+            if (entrar)
+            {
+                while (true)
+                {
+                    switch (Menu())
+                    {
+                        case EnumMenu.Consultar:
+                            break;
+                        case EnumMenu.Depositar:
+                            break;
+                        case EnumMenu.Retirar:
+                            break;
+                        case EnumMenu.Salir:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+        }
+
+        public enum EnumMenu
+        {
+            Consultar = 1, Depositar, Retirar, Salir
+        }
+
+        static EnumMenu Menu()
+        {
+            Console.WriteLine("1. Consultar saldo");
+            Console.WriteLine("2. Depositar");
+            Console.WriteLine("3. Retirar");
+            Console.WriteLine("4. Salir");
+            EnumMenu opcion = (EnumMenu)Convert.ToInt32(Console.ReadLine());
+            return opcion;
+
+        }
+
+        static bool loggin()
+        {
+            Console.WriteLine("Dame ");
+            string pin = Console.ReadLine(); 
+            if (pin == "1234")
+                return true;
+            else
+                return false;
+        }
+    }
+}
